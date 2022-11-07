7cf8b2c9b0fb4fafeb9dfba89a19af1ef90f9fa2
unknown <sebzh29@outlook.com>
2022-11-07 20:15:19 +0100

    ajout fonction deplacement avec fleches clavier

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onload = function()
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas); 
         ctx = canvas.getContext('2d'); 
-        snakee = new Snake([[6,4], [5,4], [4,4]]);
+        snakee = new Snake([[6,4], [5,4], [4,4]], "right");
         refreshCanvas();   
     }
 
@@ -37,9 +37,10 @@ window.onload = function()
         ctx.fillRect(x, y, blockSize, blockSize);
     }
 
-    function Snake(body)
+    function Snake(body, direction)
     {
         this.body = body;
+        this.direction = direction;
         this.draw = function()
         {
             ctx.save();
@@ -54,10 +55,71 @@ window.onload = function()
         this.advance = function()
         {
             var nextPosition = this.body[0].slice();
-            nextPosition[0] += 1;
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -=1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw("Invalid direction");
+            }
             this.body.unshift(nextPosition);
             this.body.pop();
         };
+        this.setDirection = function(newDirection)
+        {
+            var allowedDirections;
+            switch(this.direction)
+            {
+                case "left":                    
+                case "right":
+                    allowedDirections = ["up","down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left","right"];
+                    break;
+                default:
+                    throw("Invalid direction");
+            }
+            if(allowedDirections.indexOf(newDirection) > -1)
+            {  
+                this.direction = newDirection;
+            }
+        };
+    }
+
+    document.onkeydown = function handleKeydown(e)
+    {
+        var key = e.keyCode;
+        var newDirection;
+        switch(key)
+        {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
+        snakee.setDirection(newDirection);
     }
 }
 
