96e2842498784bc759e969695587793882cc5725
unknown <sebzh29@outlook.com>
2022-11-09 11:25:56 +0100

    ajout de style css dans le js

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ window.onload = function()
         var canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = "1px solid";
+        canvas.style.border = "30px solid #3d3d3d";
+        canvas.style.margin = "50px auto";
+        canvas.style.display = "block";
+        canvas.style.backgroundColor = "#ddd";
         document.body.appendChild(canvas); 
         ctx = canvas.getContext('2d'); 
         snakee = new Snake([[6,4], [5,4], [4,4], [3,4], [2,4]], "right");
@@ -48,10 +51,10 @@ window.onload = function()
                 }
                 while(applee.isOnSnake(snakee))
             }
-            ctx.clearRect(0, 0,canvasWidth, canvasHeight);         
+            ctx.clearRect(0, 0,canvasWidth, canvasHeight);  
+            drawScore();       
             snakee.draw();
-            applee.draw();
-            drawScore();
+            applee.draw();            
             setTimeout(refreshCanvas, delay);
         };
         
@@ -60,8 +63,19 @@ window.onload = function()
     function gameOver()
     {
         ctx.save();
-        ctx.fillText("Game Over", 5, 15);
-        ctx.fillText("Appuyer sur la touche Espace pour rejouer", 5, 30);
+        ctx.font = "bold 70px sans-serif";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 5;
+        var centreX = canvasWidth / 2;
+        var centreY = canvasHeight / 2;
+        ctx.strokeText("Game Over", centreX, centreY - 180);
+        ctx.fillText("Game Over", centreX, centreY - 180);
+        ctx.font = "bold 30px sans-serif";
+        ctx.strokeText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120);
+        ctx.fillText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120 );
         ctx.restore();
     }
 
@@ -76,7 +90,14 @@ window.onload = function()
     function drawScore()
     {
         ctx.save();
-        ctx.fillText(score.toString(), 5, canvasHeight -5);
+        ctx.font = "bold 50px sans-serif";
+        ctx.fillStyle = "gray";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        var centreX = canvasWidth / 2;
+        var centreY = canvasHeight / 2;
+
+        ctx.fillText(score.toString(), centreX, centreY);
         ctx.restore();
     }
 
